7831e765ffcc86376857bbed992b7677affe6b65
camilo_workspace_model/controller/service <camilovsprogram355@gmail.com>
2025-07-02 20:57:17 -0400

    Integracion de hateoas/swagger

--- gestion-inventario/src/main/java/com/perfulandia/inventario/dto/InventarioDTO.java
+++ gestion-inventario/src/main/java/com/perfulandia/inventario/dto/InventarioDTO.java
@@ -1,11 +1,12 @@
 package com.perfulandia.inventario.dto;
 
 import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class InventarioDTO {
+public class InventarioDTO extends RepresentationModel<InventarioDTO> {
     private Integer idInventario;
     private Integer idProducto;
     private Integer stockDisponible;
